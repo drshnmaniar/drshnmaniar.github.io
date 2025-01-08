2578f2816fe453a8f0bdf30681c464e630b191c4
Darshan Maniar <129635799+drshnmaniar@users.noreply.github.com>
2025-01-08 21:03:04 +0530

    Added skill section and print functionality

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,26 +1,42 @@
 import "../style/resume.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faPhone, faPrint } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 import resumeData from "../content/resume.json";
+const skills = resumeData.skills;
 const workExperience = resumeData.workExperience;
 const education = resumeData.education;
 
 const EmailIcon = <FontAwesomeIcon icon={faEnvelope} />;
 const PhoneIcon = <FontAwesomeIcon icon={faPhone} />;
 const LinkedinIcon = <FontAwesomeIcon icon={faLinkedin} />;
-const GithubIcon = <FontAwesomeIcon icon={faGithub} />;
+// const GithubIcon = <FontAwesomeIcon icon={faGithub} />;
+const PrintButton = () => {
+    const handlePrint = () => {
+        // Print the page
+        window.print();
+    }
+
+    return (
+        <FontAwesomeIcon icon={faPrint} className="fn-print" onClick={handlePrint} aria-label="Print Resume" title="Print Resume" />
+    );
+}
 
 const Resume = () => {
+
     return (
         <>
             <header>
-                <p className="h1">{resumeData.name} - {resumeData.position}</p>
+                <p className="h1">{resumeData.name} - {resumeData.position} <PrintButton /></p>
                 <br className="print-break" />
-                <p> {EmailIcon} <a href={`mailto:${resumeData.email}`}>{resumeData.email}</a> | {PhoneIcon} <a href={`tel:${resumeData.phone}`}>{resumeData.phone}</a> | {LinkedinIcon} <a href={resumeData.linkedinUrl} target="_blank" rel="noopener noreferrer">{resumeData.linkedinUsername}</a> | {GithubIcon} <a href={resumeData.githubUrl} target="_blank" rel="noopener noreferrer">{resumeData.githubUsername}</a></p>
+                <p> {EmailIcon} <a href={`mailto:${resumeData.email}`}>{resumeData.email}</a> | {PhoneIcon} <a href={`tel:${resumeData.phone}`}>{resumeData.phone}</a> | {LinkedinIcon} <a href={resumeData.linkedinUrl} target="_blank" rel="noopener noreferrer">{resumeData.linkedinUsername}</a>
+                    {/* | {GithubIcon} <a href={resumeData.githubUrl} target="_blank" rel="noopener noreferrer">{resumeData.githubUsername}</a> */}
+                </p>
             </header>
             <main>
+                <Skill skills={skills} />
+                <br className="print-break" />
                 <WorkExperience workExperience={workExperience} />
                 <br className="print-break" />
                 <Education education={education} />
@@ -30,6 +46,17 @@ const Resume = () => {
     )
 }
 
+const Skill = ({ skills, ...otherProps }) => {
+    return (
+        <section>
+            <h2>Skills</h2>
+            <div>
+                <p><span className="bold">Languages</span>: {skills.languages}</p>
+                <p><span className="bold">Technologies</span>: {skills.technologies}</p>
+            </div>
+        </section>
+    );
+}
 const WorkExperience = ({ workExperience, ...otherProps }) => {
     return (
         <section>
